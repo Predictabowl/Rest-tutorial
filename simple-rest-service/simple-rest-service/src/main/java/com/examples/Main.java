b40f6565b75afd96dc822277a415e5f8287f72a7
Piero Aldinucci <piero.aldinucci@gmail.com>
2020-05-12 18:58:46 +0200

    Fixed the problem with docker image.
    In reality it was the IP address, which was still hard-coded as
    localhost. Now it's properly 0.0.0.0

--- simple-rest-service/simple-rest-service/src/main/java/com/examples/Main.java
+++ simple-rest-service/simple-rest-service/src/main/java/com/examples/Main.java
@@ -25,7 +25,9 @@ import javax.inject.Singleton;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/myapp/";
+    public static final String BASE_URI = "http://0.0.0.0:"
+    		+System.getProperty("simple.rest.service.port","8080")
+    		+"/myapp/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
